84c45018d2a766b069dab2306de17956cc6ae405
Mire27 <mirepatel.19bcp080@gmail.com>
2022-11-05 16:47:28 +0530

    first commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Note Header</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ipsum!</p>
+    </div>
+  );
+}
+
+export default Notes;
